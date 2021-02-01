8d96bc8b7ca7bdda47cc18d74d00ccb691ff7328
jkelly101 <jenLkelly@gmail.com>
2021-01-31 22:02:04 -0500

    updated dynamically created elements

--- script.js
+++ script.js
@@ -2,14 +2,41 @@ var APIkey = "ec7e9482552d5ccb105e641cf33561ab";
 
 var searchHistoryArr = [];
 
-function searchLocation() {
+getLastCity();
+
+function searchLocation(event) {
+  //   console.log(event.target);
+
   var citySearch = $("#userInput").val().trim();
-  if (citySearch === "") {
-    return;
+  //   console.log(citySearch);
+
+  if (event.target.matches(".cities")) {
+    // console.log("cities");
+    citySearch = $(event.target).text();
   }
+
   getWeather(citySearch);
   getForecast(citySearch);
-  console.log(citySearch);
+
+  if (
+    event.target.matches(".search-button") ||
+    event.target.matches(".fa-search")
+  ) {
+    storeCity(citySearch);
+  }
+}
+
+function storeCity(city) {
+  searchHistoryArr.unshift(city);
+  localStorage.setItem("cityData", city);
+  var searchedCity = $("<div>").addClass("cities").text(city);
+  $(".search-history").prepend(searchedCity);
+}
+
+function getLastCity() {
+  var lastCity = localStorage.getItem("cityData");
+  getWeather(lastCity);
+  getForecast(lastCity);
 }
 
 function getWeather(search) {
@@ -19,9 +46,7 @@ function getWeather(search) {
     url: queryUrl,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
     $("#current-city").text(response.name);
-    // $("#search-history").text(response.name);
   });
 }
 
@@ -33,7 +58,13 @@ function getForecast(search) {
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    $("#temp").html("<b>Temperature: </b>" + "" + (response.main.temp + " °F"));
+    var weatherIcon = $("<img>").attr({
+      src: `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`,
+      alt: `weather icon`,
+      style: "display: block",
+    });
+    $("#current-city").append(weatherIcon);
+    $("#temp").html(`<b>Temperature: </b>${response.main.temp} °F`);
     $("#humidity").html(
       "<b>Humidity: </b>" + "" + (response.main.humidity + "%")
     );
@@ -52,18 +83,32 @@ function getFiveDay(latt, long) {
     url: queryUrl,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
+    // console.log(response);
     $("#uv-index").html("<b>UVI: </b>" + "" + response.daily[0].uvi);
 
     // Get 5-Day
     var fiveDay = response.daily;
     for (var i = 1; i < 6; i++) {
-      console.log(fiveDay[i]);
+      //   console.log(fiveDay[i]);
       var dayDiv = $("<div>").addClass("five-day-forecast");
       var date = new Date(fiveDay[i].dt * 1000).toLocaleDateString("en-US");
-      console.log(date);
+      //   console.log(date);
+
+      var weatherIcon = $("<img>").attr({
+        src: `http://openweathermap.org/img/wn/${fiveDay[i].weather[0].icon}@2x.png`,
+        alt: `weather icon`,
+      });
+
+      var temp = $("<p>").html(
+        `<b>Temperature: </b> ${fiveDay[i].temp.day} °F`
+      );
+      var humidity = $("<p>").html(`<b>Humidity: <b> ${fiveDay[i].humidity} %`);
+      dayDiv.append(date, weatherIcon, temp, humidity);
+
+      $(".future-forecast").append(dayDiv);
     }
   });
 }
 
 $(".search-button").on("click", searchLocation);
+$(".search-history").on("click", searchLocation);
